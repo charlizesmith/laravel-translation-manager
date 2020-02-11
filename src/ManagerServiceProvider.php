d136a722dec7186ab246889f802139a11bab0cbd
charlizesmith <charlizesmithweb@gmail.com>
2020-02-11 15:26:29 +0530

    Update ManagerServiceProvider.php

--- src/ManagerServiceProvider.php
+++ src/ManagerServiceProvider.php
@@ -71,28 +71,7 @@ class ManagerServiceProvider extends ServiceProvider {
         $this->publishes([
             $migrationPath => base_path('database/migrations'),
         ], 'migrations');
-
-
-        $config = $this->app['config']->get('translation-manager.route', []);
-        $config['namespace'] = 'Barryvdh\TranslationManager';
-
-        $router->group($config, function($router)
-        {
-            $router->get('view/{groupKey?}', 'Controller@getView')->where('groupKey', '.*');
-            $router->get('/{groupKey?}', 'Controller@getIndex')->where('groupKey', '.*');
-            $router->post('/add/{groupKey}', 'Controller@postAdd')->where('groupKey', '.*');
-            $router->post('/edit/{groupKey}', 'Controller@postEdit')->where('groupKey', '.*');
-            $router->post('/groups/add', 'Controller@postAddGroup');
-            $router->post('/delete/{groupKey}/{translationKey}', 'Controller@postDelete')->where('groupKey', '.*');
-            $router->post('/import', 'Controller@postImport');
-            $router->post('/find', 'Controller@postFind');
-            $router->post('/locales/add', 'Controller@postAddLocale');
-            $router->post('/locales/remove', 'Controller@postRemoveLocale');
-            $router->post('/publish/{groupKey}', 'Controller@postPublish')->where('groupKey', '.*');
-            $router->post('/translate-missing', 'Controller@postTranslateMissing');
-	    $router->post('/changetable', 'Controller@postChangeTable');
-        });
-
+        
         $this->loadRoutesFrom(__DIR__.'/routes.php');
 
 	}
